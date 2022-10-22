4eb2da286291f84210c2ef6d6fb3b7b82e77cdaf
MiersX <michael.muirhead123@gmail.com>
2022-10-22 02:02:15 +0000

    generate controller for about, add header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'products#index'
+  root "products#index"
+
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -12,10 +13,14 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :about, only: [:index]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
